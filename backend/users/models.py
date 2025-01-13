525e18b1aa3734807fd6aa1b6405b7a10b2b1324
Polina Tutygina <polina.tutygina@yandex.ru>
2025-01-13 20:54:07 +0300

    Fixed the user model.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 
@@ -6,6 +6,16 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
     subscriptions = models.ManyToManyField(
         'self', symmetrical=False, related_name='subscribers', blank=True
+    )
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions',
+        blank=True
     )
 
     def __str__(self):
